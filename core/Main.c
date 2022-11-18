f5eaf704894cb4ea512ebc030ed15c718a475b35
Chrisser1 <chrisgthomsen0310@gmail.com>
2022-11-18 13:04:18 +0100

    idk

--- core/Main.c
+++ core/Main.c
@@ -1,4 +1,20 @@
+#define BIT(x) (1 << (x))
+#define SETBITS(x,y) ((x) |= (y))
+#define CLEARBITS(x,y) ((x) &= (~(y)))
+#define SETBIT(x,y) SETBITS((x), (BIT((y))))
+#define CLEARBIT(x,y) CLEARBITS((x), (BIT((y))))
+#define BITVAL(x,y) (((x)>>(y)) & 1)
+#define HERTZ(x) ((CPU_CLOCK/400)/2)
+
+#include <avr/io.h>
+#include <util/delay.h>
+
 int main(int argc, char const *argv[]){
   /* code */
   return 0;
 }
+
+void ini(){
+	DDRB = 0x06; 
+}
+

90cd6aa2c076ae44e8a756e714448c820b1b4360
Chrisser1 <chrisgthomsen0310@gmail.com>
2022-11-24 13:34:12 +0100

    GUI_Changes

--- python/GUI/StreamlitMain.py
+++ python/GUI/StreamlitMain.py
@@ -1,1 +1,49 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+def save_data():
+  data = {'ID': [1],
+            'Gender': [Gender],
+            'Age': [Age],
+            'HearingLoss': [HearingLoss],
+            'HeadphoneTime': [HeadphoneTime],
+            'PlaceOfTest': [PlaceOfTest],
+            'Name': [Name]}
+  df = pd.DataFrame(data)
+  df.to_csv('ID_collection.csv', mode='a', index=False, header=False)
+
+st.title('The bedst hearing test online:')
+
+col1, col2 = st.columns(2)
+with col1:
+  st.header('Indput data here')
+  Name = st.text_input('Name')
+  Gender = st.selectbox('Gender',('Man','Whoman'))
+  Age = st.number_input('Age',min_value=0,max_value=150)
+  HearingLoss = st.selectbox('HearingLoss',('have been diagnosed with hearingLoss','do not have hearingLoss'))
+  HeadphoneTime = st.number_input('HeadphoneTime in hours/day',min_value=0,max_value=24)
+  PlaceOfTest = st.text_input('PlaceOfTest')
+  st.header('Tester have to do the rest :smile:')
+  Start = st.number_input('Manual input 1',value=2500.0)
+  Slut = st.number_input('Manual input 2',value=17500.0)
+  values = st.slider(
+    'Select a range of frequency',
+    0.0, 20000.0, value=(Start,Slut))
+  st.write('Values:', values)
+with col2:
+  st.header('See output')
+  st.write('Currently getting data from: ',Name)
+  st.write('The gender is: ',Gender)
+  st.write('The age is: ',Age)
+  if Age == 69:
+    st.write('Nice :smile:')
+  st.write('The one in question ',HearingLoss)
+  st.write(Name,' uses the headphone: ',HeadphoneTime,'hour/day')
+  st.write('The test is taken at: ',PlaceOfTest)
+  Starter = st.button('Start test')
+  if Starter:
+    st.write('Test has been started :smile:')
+    save_data()
+
+
+
--- python/Visualization/TreatedData.py
+++ python/Visualization/TreatedData.py
@@ -7,18 +7,18 @@ Svar = ["None","Left","Right","Both"]
 Ear = ["None","Left","Right","Both"]
 
 def Change_Dataform():
-  with open('NewResultStorage.csv', 'a') as f_object:
-    for j in range(0,len(Re)):
-      Out = Re.loc[j,'LeftOUT']+Re.loc[j,'RightOUT']*2
-      Response = Re.loc[j,'LeftResponse']+Re.loc[j,'RightResponse']*2
-      data = {'ID': [Re.loc[j,'ID']],
-              'Frekvens': [Re.loc[j,'Frekvens']],
-              'Out': [Ear[Out]],
-              'Response': [Svar[Response]],
-              'AnswerTime': [Re.loc[j,'AnswerTime']]}
-      df = pd.DataFrame(data)
-      #append data frame to CSV file
-      df.to_csv('NewResultStorage.csv', mode='a', index=False, header=False)
+  # with open('NewResultStorage.csv', 'a') as f_object:
+  for j in range(0,len(Re)):
+    Out = Re.loc[j,'LeftOUT']+Re.loc[j,'RightOUT']*2
+    Response = Re.loc[j,'LeftResponse']+Re.loc[j,'RightResponse']*2
+    data = {'ID': [Re.loc[j,'ID']],
+            'Frekvens': [Re.loc[j,'Frekvens']],
+            'Out': [Ear[Out]],
+            'Response': [Svar[Response]],
+            'AnswerTime': [Re.loc[j,'AnswerTime']]}
+    df = pd.DataFrame(data)
+    #append data frame to CSV file
+    df.to_csv('NewResultStorage.csv', mode='a', index=False, header=False)
 
 def Clear_Data():
   f = open("NewResultStorage.csv","w")

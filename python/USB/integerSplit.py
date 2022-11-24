9c771d02f81c2dd87f45f455d49c3fb990ee042c
dendersen <dendersen@endersen.dk>
2022-11-24 12:16:59 +0100

    fix?

--- python/USB/integerSplit.py
+++ python/USB/integerSplit.py
@@ -2,6 +2,7 @@ def numberTo16Bit(num:int) -> str:
   out:str = []
   out += (chr((num - num%256)//256))
   out += (chr(num%256))
+  out = out.encode("ascii","ignore")
   return str(out)
 
 def numberFrom16Bit(text:str) -> int:
